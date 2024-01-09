1fb5ea6eb5c7cbf833de16facf9ec5566082b7ff
Ryanwalker277 <andianurag277@gmail.com>
2024-01-09 18:44:21 +0530

    Feat: Added Healthchecks for services

---
+++ sample/01-all-features/src/health/health.interface.ts
@@ -0,0 +1,19 @@
+export interface DatabaseConnectionDetails {
+    name: string;
+    type: 'postgres';
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+    database: string;
+  }
+  
+  export interface FusionAuthConnectionDetails {
+    healthEndpoint: string;
+  }
+  
+  export interface RedisConnectionDetails {
+    host: string;
+    port: number;
+  }
+  
