e5615c8321b011d63c6ff766230181efd5b2504f
Darren Dsouza <darrendsouza7273@gmail.com>
2024-06-24 01:17:43 +0530

    fix: Consists of Suggested changes and even added test cases

--- packages/common/src/interceptors/index.ts
+++ packages/common/src/interceptors/index.ts
@@ -2,3 +2,4 @@ export * from './response-time.interceptor';
 export * from './response-format.interceptor';
 export * from './file-upload.interceptor';
 export * from './utils';
+export * from './geoip.interceptor';
--- sample/07-geopip-blocking/src/main.ts
+++ sample/07-geopip-blocking/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { GeoIPInterceptor } from './geoip.interceptor';
+import { GeoIPInterceptor } from '@samagra-x/stencil';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
