b9a9d2ed45f0ec98c2245cfdeccf59881121b744
Darren Dsouza <darrendsouza7273@gmail.com>
2024-06-26 19:05:16 +0530

    fix: Updated File Upload

--- packages/common/src/controllers/file-upload.controller.ts
+++ packages/common/src/controllers/file-upload.controller.ts
@@ -9,82 +9,52 @@ import {
   Res,
   Body,
   Query,
+  Logger,
+  ValidationPipe,
+  InternalServerErrorException,
 } from '@nestjs/common';
-import { FastifyFileInterceptor, FastifyFilesInterceptor } from '../interceptors/file-upload.interceptor';
+import { FastifyFilesInterceptor} from '../interceptors/file-upload.interceptor';
 import { MultipartFile } from '../interfaces/file-upload.interface';
 import { FileUploadService } from '../services/file-upload.service';
 import { FastifyReply } from 'fastify';
 
+
 interface UploadFilesDto {
-  filenames: string[];
+ filename: string[];
 }
 
 @Controller('files')
 export class FileUploadController {
+  private readonly logger = new Logger(FileUploadController.name);
   constructor(private readonly filesService: FileUploadService) {}
 
-  @Post('upload-file')
-  @UseInterceptors(FastifyFileInterceptor('file', {}))
-  async uploadFile(
-    @UploadedFile() file: MultipartFile,
-    @Query('destination') destination: string,
-    @Query('filename') filename: string,
-  ): Promise<{
-    statusCode?: number;
-    message: string;
-    file?: { url: string } | undefined;
-  }> {
-    try {
-      const directory = await this.filesService.upload(
-        file,
-        destination,
-        filename,
-      );
-      return {
-        message: 'File uploaded successfully',
-        file: { url: directory },
-      };
-    } catch (error) {
-      console.error(`Error uploading file: ${error.message}`);
-      return {
-        statusCode: 500,
-        message: 'File upload failed',
-        file: undefined,
-      };
-    }
-  }
-
   @Post('upload-files')
-  @UseInterceptors(FastifyFilesInterceptor('files', 10))
-  async uploadMultipleFiles(
-    @UploadedFiles() files: ReadonlyArray<MultipartFile>,
-    @Query('destination') destination: string,
-    @Body() body: UploadFilesDto,
-  ): Promise<{
-    statusCode?: number;
-    message: string;
-    files?: { url: any }[] | undefined;
-  }> {
-    try {
-      const { filenames } = body; // Extract filenames from form data
-      const directories = await this.filesService.uploadMultiple(
-        files,
-        destination,
-        filenames,
-      );
-      return {
-        message: 'Files uploaded successfully',
-        files: directories.map((directory) => ({ url: directory })),
-      };
-    } catch (error) {
-      console.error(`Error uploading files: ${error.message}`);
-      return {
-        statusCode: 500,
-        message: 'Files upload failed',
-        files: undefined,
-      };
-    }
-  }
+  @UseInterceptors(FastifyFilesInterceptor('file', []))
+     async uploadMultipleFiles(
+       @UploadedFiles() file: ReadonlyArray<MultipartFile>,
+       @Query('destination') destination: string,
+       @Body() body: UploadFilesDto,
+     ): Promise<{
+       statusCode?: number;
+       message: string;
+       files?: { url: any }[] | undefined;
+     }> {
+       try {
+         const { filename } = body;
+         const directories = await this.filesService.uploadMultiple(
+           file,
+           destination,
+           filename,
+         );
+         return {
+           message: 'Files uploaded successfully',
+            files: directories.map((directory) => ({ url: directory })),
+         };
+       } catch (error) {
+         this.logger.error(`Error uploading files: ${error.message}`);
+         throw new InternalServerErrorException('File upload Failed');
+       }
+     }
 
 
   @Get('download/:destination')
@@ -100,9 +70,8 @@ export class FileUploadController {
       });
       fileStream.pipe(res.raw);
     } catch (error) {
-      console.log('error: ', error);
-      console.error(`Error downloading file: ${error.message}`);
-      res.status(500).send('File download failed');
-    }
+      this.logger.error(`Error downloading file: ${error.message}`);
+      throw new InternalServerErrorException('Error downloading file');
   }
+ }
 }
