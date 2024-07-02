d7b726c7e6f441bde4540f09486751d372f0cbcd
Savio Dias <diassavio629@gmail.com>
2024-07-02 13:36:57 +0000

    fix: changed variable name

--- packages/common/src/interceptors/response-format.interceptor.ts
+++ packages/common/src/interceptors/response-format.interceptor.ts
@@ -13,37 +13,30 @@ import { catchError, map } from 'rxjs/operators';
 export class ResponseFormatInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      map((data) => ({
-        success: true,
-        statusCode: context.switchToHttp().getResponse().statusCode,
-        message: context.switchToHttp().getRequest().method === 'POST' ? 'Created' : 'Success',
-        data,
-        error: {},
-      })),
+      map((response) => {
+        return {
+          success: true,
+          statusCode: context.switchToHttp().getResponse().statusCode,
+          data: response.message, 
+          error: null,
+        };
+      }),
       catchError((error) => {
         const status = error instanceof HttpException ? error.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
-        const message = this.formatErrorMessage(error);
+        const errMessage = this.formatErrorMessage(error);
 
         return of({
           success: false,
           statusCode: status,
-          message,
           data: null,
-          error: this.formatError(status, message),
+          error: errMessage,
         });
       }),
     );
   }
 
   private formatErrorMessage(error: any): string {
-    const message = error?.response?.message || error.message || 'Internal Server Error';
-    return Array.isArray(message) ? message.join(', ') : message;
-  }
-
-  private formatError(status: number, message: string): any {
-    return {
-      code: status,
-      message,
-    };
+    const errMessage = error?.response?.message || error.message || 'Internal Server Error';
+    return Array.isArray(errMessage) ? errMessage.join(', ') : errMessage;
   }
 }
