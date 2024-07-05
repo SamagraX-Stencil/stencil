07c6211c54b3784d110052b66c7373f85a8eb70b
RiyaRaj28 <rrajj7674@gmail.com>
2024-07-05 12:52:50 +0530

    updated in dev

--- packages/common/src/interceptors/dto/file-upload.dto.ts
+++ packages/common/src/interceptors/dto/file-upload.dto.ts
@@ -1,7 +1,3 @@
-import { Multer } from "multer";
-
-export type MulterInstance = Multer;
-
 export type InterceptResponseDTO = {
     observable : { [fieldName: string]: Express.Multer.File }; 
   } 
