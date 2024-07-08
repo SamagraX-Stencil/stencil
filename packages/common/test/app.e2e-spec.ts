7bb06ee5d0c35652a44edda486f7067c9b44bf0a
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-07-08 16:00:57 +0530

    fix wrong text in e2e spec

--- packages/common/test/app.e2e-spec.ts
+++ packages/common/test/app.e2e-spec.ts
@@ -21,7 +21,7 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World from file upload.');
+      .expect('Hello World!');
   });
 
   it('/files/upload-file (POST); for file with content', async () => {
