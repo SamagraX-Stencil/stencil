cdb5fe83e8216c967acb0e9ee9aa35ab2d92a875
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-07-12 11:21:54 +0530

    tests for empty file upload

--- sample/04-file-upload/test/app.e2e-spec.ts
+++ sample/04-file-upload/test/app.e2e-spec.ts
@@ -2,8 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-// import * as path from 'path';
-// import * as fs from 'fs';
+import * as path from 'path';
+import * as fs from 'fs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -24,67 +24,56 @@ describe('AppController (e2e)', () => {
       .expect('Hello World from file upload.');
   });
 
-  // it('/files/upload-file (POST); for file with content', async () => {
-  //   const testFilePath = path.join(__dirname, 'empty.txt');
+  it('/files/upload-file (POST); for file with content', async () => {
+    const testFilePath = path.join(__dirname, 'empty.txt');
 
-  //   // Create an empty.txt file in the same directory as the test
-  //   fs.writeFileSync(testFilePath, 'abcd');
+    // Create an empty.txt file in the same directory as the test
+    fs.writeFileSync(testFilePath, 'abcd');
+    console.log(__dirname);
+    const mockDestination = 'uploads';
+    const mockFilename = 'content.txt';
 
-  //   const mockDestination = 'uploads';
-  //   const mockFilename = 'empty.txt';
+    const response = await request(app.getHttpServer())
+      .post(
+        `/files/upload-file?destination=${mockDestination}&filename=${mockFilename}`,
+      )
+      .attach('file', Buffer.from('content'), mockFilename);
 
-  //   const response = await request(app.getHttpServer())
-  //     .post(
-  //       `/files/upload-file?destination=${mockDestination}&filename=${mockFilename}`,
-  //     )
-  //     .set('User-Agent', 'PostmanRuntime/7.40.0')
-  //     .set('Accept', '*/*')
-  //     .set('Cache-Control', 'no-cache')
-  //     .set('Postman-Token', '67bdd903-9c93-4c8a-bf5b-0e0072c22f28')
-  //     .set('Host', 'localhost:3000')
-  //     .set('Accept-Encoding', 'gzip, deflate, br')
-  //     .set('Connection', 'keep-alive')
-  //     .set(
-  //       'Content-Type',
-  //       'multipart/form-data; boundary=--------------------------147778240219837777371846',
-  //     )
-  //     .attach('file', testFilePath);
+    console.log('Response:', response.body);
 
-  //   console.log('Response:', response.body);
+    expect(response.body).toEqual({
+      message: 'File uploaded successfully',
+      file: { url: `${mockDestination}/${mockFilename}` },
+    });
 
-  //   expect(response.body).toEqual({
-  //     message: 'File uploaded successfully',
-  //     file: { url: `${mockDestination}/${mockFilename}` },
-  //   });
+    // Clean up created file from the test directory
+    fs.unlinkSync(testFilePath);
 
-  //   // Clean up created file from the test directory
-  //   fs.unlinkSync(testFilePath);
+    // Clean up the uploaded file from the uploads directory
+    const uploadedFilePath = path.join(
+      __dirname,
+      `../${mockDestination}/${mockFilename}`,
+    );
+    if (fs.existsSync(uploadedFilePath)) {
+      fs.unlinkSync(uploadedFilePath);
+    }
+  });
 
-  //   // Clean up the uploaded file from the uploads directory
-  //   const uploadedFilePath = path.join(
-  //     __dirname,
-  //     `../${mockDestination}/${mockFilename}`,
-  //   );
-  //   if (fs.existsSync(uploadedFilePath)) {
-  //     fs.unlinkSync(uploadedFilePath);
-  //   }
-  // });
+  it('/files/upload-file (POST); for empty file check', async () => {
+    const mockDestination = 'uploads';
+    const mockFilename = 'empty.txt';
 
-  // it('/files/upload-file (POST); for empty file check', async () => {
-  //   const mockDestination = 'uploads';
-  //   const mockFilename = 'empty.txt';
+    const response = await request(app.getHttpServer())
+      .post(
+        `/files/upload-file?destination=${mockDestination}&filename=${mockFilename}`,
+      )
+      .attach('file', Buffer.from(''), mockFilename);
 
-  //   const response = await request(app.getHttpServer())
-  //     .post(
-  //       `/files/upload-file?destination=${mockDestination}&filename=${mockFilename}`,
-  //     )
-  //     .attach('file', Buffer.from(' '), mockFilename);
+    console.log('Response:', response.body);
 
-  //   console.log('Response:', response.body);
-
-  //   expect(response.body).toEqual({
-  //     statusCode: 400,
-  //     message: 'empty file uploads are not allowed',
-  //   });
-  // });
+    expect(response.body).toEqual({
+      statusCode: 400,
+      message: 'empty file uploads are not allowed',
+    });
+  });
 });
